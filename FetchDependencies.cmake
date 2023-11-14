43afd8570c1c2b931e87ae85a0f01713820ac1a7
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2023-11-14 20:53:51 +0200

    Fetch googletest and gflags libs

--- FetchDependencies.cmake
+++ FetchDependencies.cmake
@@ -8,5 +8,19 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(zlib)
 set(ZLIB_INCLUDE_DIR ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
 
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        v1.14.0
+)
+FetchContent_MakeAvailable(googletest)
+
+FetchContent_Declare(
+  gflags
+  GIT_REPOSITORY https://github.com/gflags/gflags
+  GIT_TAG        v2.2.2
+)
+FetchContent_MakeAvailable(gflags)
+
 set(DEPENDENCIES_LIBS zlib)
 set(DEPENDENCIES_INCLUDES ${ZLIB_INCLUDE_DIR})
