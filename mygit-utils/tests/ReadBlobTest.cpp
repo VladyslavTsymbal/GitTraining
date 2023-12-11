ad4f43f751c389899f974b5bfddab0468c3b0a5d
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2023-12-11 15:06:02 +0200

    Add test for readBlob func with real file

--- mygit-utils/tests/ReadBlobTest.cpp
+++ mygit-utils/tests/ReadBlobTest.cpp
@@ -4,6 +4,8 @@
 
 #include <sstream>
 #include <string_view>
+#include <filesystem>
+#include <fstream>
 
 TEST(ReadBlob, when_valid_compressed_data_and_hash_passed_then_initial_data_received)
 {
@@ -15,6 +17,31 @@ TEST(ReadBlob, when_valid_compressed_data_and_hash_passed_then_initial_data_rece
 
     std::stringstream output_ss;
     if (git::readBlob(input_ss, output_ss))
+    {
+        EXPECT_TRUE(false);
+    }
+
+    const std::string uncompressed_data = output_ss.str();
+    const std::string_view uncompressed_data_view { uncompressed_data };
+
+    const auto null_pos = uncompressed_data.find_first_of('\0');
+    ASSERT_NE(null_pos, std::string::npos);
+
+    const std::string_view body = uncompressed_data_view.substr(null_pos + 1);
+    const std::string_view expected_uncompressed_data = "what is up, Mykhailo?";
+    ASSERT_STREQ(body.data(), expected_uncompressed_data.data());
+}
+
+TEST(ReadBlob, when_valid_test_blob_file_passed_and_then_initial_data_received)
+{
+    const std::filesystem::path filepath= "../../mygit-utils/tests/GitTestBlob";
+    ASSERT_TRUE(std::filesystem::exists(filepath));
+
+    std::ifstream input_file(filepath, std::ios::binary);
+    ASSERT_TRUE(input_file.is_open());
+
+    std::stringstream output_ss;
+    if (git::readBlob(input_file, output_ss))
     {
         EXPECT_TRUE(false);
     }
